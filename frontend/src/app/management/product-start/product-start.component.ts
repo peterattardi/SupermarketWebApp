28b43d0baaa5ce546b2c397655b5366e31e64c90
Riccardo Sarro <riccardo.2s@icloud.com>
2021-05-31 04:35:49 +0200

    Add: Products Management, Admin Area, Done the structure, miss details

---
+++ frontend/src/app/management/product-start/product-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-start',
+  templateUrl: './product-start.component.html',
+  styleUrls: ['./recipe-start.component.css']
+})
+export class ProductStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

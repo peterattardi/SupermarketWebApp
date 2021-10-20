a0cd248b19cb5117912e7adfbdc1b2d5ac8ab100
Riccardo Sarro <64036946+Riccardo2s@users.noreply.github.com>
2021-10-20 16:43:52 +0200

    Google API key removed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API: 'http://8c587108f445.ngrok.io/',
-  GOOGLE_API_KEY: 'AIzaSyAnPPgAbmA2ec8pFbBuK59WGRxQOkKbNqo'
+  GOOGLE_API_KEY: 'your_google_api_key'
 };
